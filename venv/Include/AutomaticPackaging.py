744cf51b8088e2cb736dffc28bf35b704c9f53ab
Tiger <atlantisspeed@gmail.com>
2019-04-20 17:48:09 +0800

    优化完毕

--- venv/Include/AutomaticPackaging.py
+++ venv/Include/AutomaticPackaging.py
@@ -3,38 +3,22 @@
 
 import os
 import platform
-import shutil
-import subprocess
 import sys
-import json
+import shutil
 
-# 配置
-# 打包流程
-
-# 检查是否是 WINDOWS 系统
-#     检查 JAVA 环境是否安装
-#     检查 Android 环境是否安装
-# 检查是否是 MAC 系统
-#     检查 JAVA 环境是否安装
-#     检查 Android 环境是否安装
-# 检查是否是 Ubuntu 系统
-#     检查 JAVA 环境是否安装
-#     检查 Android 环境是否安装
-
-# 检查ok之后
-
-# 原文件
-# 目标文件
-# jks 文件
-# CheckAndroidV2Signature.jar 路径
-# walle-cli-all.jar 路径
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# D:/DevelopmentTools/Android/SDK/build-tools/28.0.0/                                                       
+android_build_tools_path = 'D:/DevelopmentTools/Android/SDK/build-tools/28.0.0/'
+target_file_name = 'AP'
+keystore_password = "123456"
+key_alias = "AutomaticPackaging"
+key_password = "123456"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 platform_platform = platform.platform()
 is_windows_platform = 'Windows' in platform_platform
 is_linux_platform = 'Linux' in platform_platform
 is_macos_platform = 'MacOS' in platform_platform
-# D:/DevelopmentTools/Android/SDK/build-tools/28.0.0/
-android_build_tools_path = 'D:/DevelopmentTools/Android/SDK/build-tools/28.0.0/'
 
 
 def current_path():
@@ -43,9 +27,6 @@ def current_path():
         return path
     elif os.path.isfile(path):
         return os.path.dirname(path)
-
-
-enev_path = current_path().split('Include')[0]
 
 
 def find_target_file_path(rootDir, file_name):
@@ -70,7 +51,7 @@ def source_apk_file_path():
 
 
 def source_target_file_path():
-    return current_path().split('Include')[0] + 'apk' + different_platforms_backslash() + 'target_.apk'
+    return current_path().split('Include')[0] + 'apk' + different_platforms_backslash() + target_file_name + '.apk'
 
 
 def jks_flie_path():
@@ -93,22 +74,6 @@ def out_put_apk_path():
     return current_path().split('Include')[0] + 'out_put_apk'
 
 
-def check_platform():
-    if 'Windows' in platform_platform:
-        return 'Windows'
-    elif 'Linux' in platform_platform:
-        return 'Linux'
-    elif 'MacOS' in platform_platform:
-        return 'MacOS'
-    else:
-        return 'Other'
-
-
-def check_java_env():
-    system = os.system('java -version 2>&1 | awk -F[\\\"_] \'NR==1{print $2}\'')
-    return system
-
-
 ##### **1.** 对齐:
 # ```
 # Win:zipalign -v 4 source.apk destination.apk
@@ -116,7 +81,8 @@ def check_java_env():
 # Mac:./zipalign -v 4 source.apk destination.apk
 # ```
 def zip_align():
-    os.system(android_build_tools_path + "zipalign -v 4 " + source_apk_file_path() + " " + source_target_file_path())
+    os.system(
+        android_build_tools_path + "zipalign -v 4 " + source_apk_file_path() + " " + source_target_file_path())
 
 
 ##### **2.** 优化:
@@ -135,48 +101,49 @@ def zip_align_optimization():
 #
 # Mac: apksigner sign --ks [你的签名文件] [apk路径]
 # ```
-keystorePath = jks_flie_path()
-keystorePassword = "xxx"
-keyAlias = "xxx"
-keyPassword = "xxx"
-
-
 def apksigner_sign():
-    # signShell = android_build_tools_path + "apksigner sign --ks "+ keystorePath + " --ks-key-alias " + keyAlias + " --ks-pass pass:" + keystorePassword + " --key-pass pass:" + keyPassword + " --out " + source_target_file_path() + " " + zipalignedApkPath
-    signShell = android_build_tools_path + "apksigner sign --ks " + keystorePath + " --ks-key-alias " + keyAlias + " --ks-pass pass:" + keystorePassword + " --key-pass pass:" + keyPassword + " --out " + source_target_file_path() + " " + source_apk_file_path()
-    os.system(signShell)
+    jks_path = jks_flie_path()
+    # apksigner_sign_shell = android_build_tools_path + "apksigner sign --ks "+ jks_path + " --ks-key-alias " + key_alias + " --ks-pass pass:" + keystore_password + " --key-pass pass:" + key_password + " --out " + source_target_file_path() + " " + source_apk_file_path()
+    apksigner_sign_shell = android_build_tools_path + "apksigner sign --ks " + jks_path + " --ks-key-alias " + key_alias + " --ks-pass pass:" + keystore_password + " --key-pass pass:" + key_password + " --out " + source_target_file_path() + " " + source_apk_file_path()
+    os.system(apksigner_sign_shell)
 
 
 #### **4.** 检查是否使用v2签名:
 # ```
 # java -jar CheckAndroidV2Signature.jar destination.apk
 # ```
+# {"ret":0,"msg":"ok","isV2":true,"isV2OK":true}
 def check_android_v2_signature():
-    checkV2Shell = "java -jar " + checkandroidv2signature_flie_path() + " " + source_target_file_path();
-    system = os.system(checkV2Shell)
-    print('检查是否使用v2签名:' + system)
-
-
-#     {"ret":0,"msg":"ok","isV2":true,"isV2OK":true}
+    check_android_v2_signature_shell = "java -jar " + checkandroidv2signature_flie_path() + " " + source_target_file_path();
+    system = os.system(check_android_v2_signature_shell)
 
 
 #### **5.** 写入渠道包信息:
 # ```
-# java -jar [刚下载walle-cli-all.jar的路径] batch -f [项目里channel的路径]  [apk路径]
+# java -jar [walle-cli-all.jar的路径] batch -f [项目里channel的路径]  [apk路径]
 # ```
 def batch_write_channel():
-    writeChannelShell = "java -jar " + walle_cli_all_flie_path() + " batch -f " + channel_flie_path() + " " + source_target_file_path() + " " + out_put_apk_path()
-    system = os.system(writeChannelShell)
+    batch_write_channel_shell = "java -jar " + walle_cli_all_flie_path() + " batch -f " + channel_flie_path() + " " + source_target_file_path() + " " + out_put_apk_path()
+    system = os.system(batch_write_channel_shell)
 
 
-if __name__ == '__main__':
-    # print(current_path().split('Include')[0] + 'apk')
-    # print(enev_path)
-    # print(find_target_file_path(current_path().split('Include')[0] + 'apk', '.apk'))
-    # print(source_apk_file_path())
-    print(source_target_file_path())
-    zip_align()
+def auto_write_channel():
+    if not os.path.exists(source_target_file_path()):
+        zip_align()
+    else:
+        os.remove(source_target_file_path())
+        zip_align()
+
     zip_align_optimization()
     apksigner_sign()
     check_android_v2_signature()
-    batch_write_channel()
+
+    if not os.path.exists(out_put_apk_path()):
+        batch_write_channel()
+    else:
+        shutil.rmtree(out_put_apk_path())
+        batch_write_channel()
+
+
+if __name__ == '__main__':
+    auto_write_channel()
